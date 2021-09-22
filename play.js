99a936898ea39f59ec7a03aa8dad23782a43ef37
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-22 22:59:43 +0000

    moving snake

--- play.js
+++ play.js
@@ -7,24 +7,37 @@ const connect = function (){   // create a variable: connect
     console.log("yes mann")); 
     conn.setEncoding("utf8"); 
 
-    conn.on("connect" , ()=>{
-      conn.write("Name: YBY"); 
+    conn.on("connect", ()=>{
+      conn.write("Name: YBY");
       console.log("Successfully connected to game server ..yadayada");
-    })
+
+      let a = ["Move: up","Move: left","Move: down","Move: right"]; 
+
+      for (let j = 0 ; j < a.length; j++ ) {
+        console.log("yyyyyyy");
+        setTimeout(()=>{
+        for (let i  = 0 ; i < 10; i++){ 
+          setTimeout(()=>{  
+            conn.write(a[j]);
+            }, 100*i)}
+          }, 1000*j)}
+      });
+
     conn.on("data", (data) => {
-      // code that does something when the connection is first established
-      console.log(data)
+      console.log(data);
     });
 
-    
-
-    // console.log("yes mann2");
-    return conn; 
-
-    
-}; 
-
-// console.log("YB connecting .... ") ; 
+    return conn;     
+};
 connect();
-
 module.exports = connect ; 
+
+        // (function(innerv, outterv) {
+        //   setTimeout(function() { 
+        //     setTimeout(()=>{  
+        //       conn.write(a[j]);
+        //       console.log("innerv: ", innerv , "outterv: ", outterv);
+        //       console.log("time: ", 50*innerv+j*10, "motion: ", a[outterv]);
+        //       }, 50*innerv)
+        //   },  outterv);
+        // })(i,j);
