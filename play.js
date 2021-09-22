75ed9768c3c6f91e719052efd6862d1a88f587e6
YBYuan007 <yuanyanbin2011@gmail.com>
2021-09-22 17:57:00 +0000

    connected to the server and return msg

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require("net") ; 
+
+const connect = function (){   // create a variable: connect
+  const conn = net.createConnection({
+    host: "135.23.223.133", 
+    port: 50542}, 
+    console.log("yes mann")); 
+    conn.setEncoding("utf8"); 
+
+    conn.on("data", (data) => {
+      // code that does something when the connection is first established
+      console.log(data)
+    });
+
+  
+
+    // console.log("yes mann2");
+    return conn; 
+
+    
+}; 
+
+// console.log("YB connecting .... ") ; 
+connect();
